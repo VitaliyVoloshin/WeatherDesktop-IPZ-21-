f45eb151fbd9d6987279aa879a01b1f9bbe19690
VitaliyVoloshin <51855930+VitaliyVoloshin@users.noreply.github.com>
2020-02-18 09:40:10 +0200

    1.0.0.0

---
+++ WeatherDesktop[ IPZ-21 ]/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WeatherDesktop__IPZ_21__
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.UpPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UpPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UpPanel
+            // 
+            this.UpPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(166)))), ((int)(((byte)(173)))));
+            this.UpPanel.Controls.Add(this.button1);
+            this.UpPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.UpPanel.Location = new System.Drawing.Point(0, 0);
+            this.UpPanel.Name = "UpPanel";
+            this.UpPanel.Size = new System.Drawing.Size(800, 40);
+            this.UpPanel.TabIndex = 0;
+            this.UpPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.UpPanel_Paint);
+            this.UpPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.UpPanel_MouseDown);
+            this.UpPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.UpPanel_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(727, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 37);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(126)))), ((int)(((byte)(171)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UpPanel);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weather Desktop";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.UpPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel UpPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WeatherDesktop[ IPZ-21 ]/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherDesktop__IPZ_21__
+{
+ 
+
+    public partial class MainForm : Form
+    {
+        Point PanelMouseDownLocation;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            UpPanel.BackColor = Color.FromArgb(150, UpPanel.BackColor);  // Setting transparency of panel
+            string lat, lon;
+            (lat, lon) = MyNetwork.GetGeoLoc();
+            string main, description, temp;
+            (main, description, temp) = MyNetwork.GetWeather(lat, lon);
+            try
+            {
+                temp = (Math.Round((Convert.ToDouble(temp.Replace('.', ',')) - 273.15),2)).ToString();
+                label1.Text = main; label2.Text = description; label3.Text = temp+" C";
+            }
+            catch (Exception e) { label1.Text = e.ToString(); }
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void UpPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void UpPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) PanelMouseDownLocation = e.Location;
+        }
+
+        private void UpPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - PanelMouseDownLocation.X;
+                this.Top += e.Y - PanelMouseDownLocation.Y;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+
+
+
+    static class MyNetwork
+    {
+        private static string API_KEY = "1216b541254a76414c87650bbd955c09";
+        public static string GetIP()
+        {
+            using (var c = new WebClient()) {
+                string x = c.DownloadString(@"https://api.myip.com");
+                var o = JObject.Parse(x);
+                return (string)o["ip"];
+            }
+        }
+
+        public static (string, string) GetGeoLoc()
+        {
+            using (var c = new WebClient()) {
+                var s = c.DownloadString(string.Format(@"http://www.geoplugin.net/json.gp?ip={0}", GetIP()));
+                var e = JObject.Parse(s);
+                return ((string)e["geoplugin_latitude"], (string)e["geoplugin_longitude"]);
+            } 
+        }
+
+        public static (string, string, string) GetWeather(string lat, string lon) {
+            using (var c = new WebClient())
+            {
+                var s = c.DownloadString(string.Format("https://api.openweathermap.org//data//2.5//weather?lat={0}&lon={1}&APPID={2}", lat, lon, API_KEY));
+                var e = JObject.Parse(s);
+                return ((string)(e["weather"][0]["main"]), (string)(e["weather"][0]["description"]), (string)(e["main"]["temp"]));
+            }
+        }
+
+    }
+
+
+}
